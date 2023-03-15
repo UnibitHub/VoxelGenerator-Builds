ac0e2775370b75e55694feda2e3c65f3b5ab92d3
v17alya <novosadvitalya@gmail.com>
2023-03-15 19:18:14 +0200

    *

--- betterspace-build/js/animated_loader.js
+++ betterspace-build/js/animated_loader.js
@@ -1,43 +1,75 @@
-// Setup the canvas element.
-var canvas = $("canvas.dots");
-var context = canvas[0].getContext("2d");
-var canvasWidth = canvas.width();
-var canvasHeight = canvas.height();
-canvas.attr({ height: canvasHeight, width: canvasWidth });
-var image_loaded = false;
-const img = new Image();
-img.src = "TemplateData/SPLASH_SCREEN.jpg";
-img.onload = () => {
-  image_loaded =true;
-  context.drawImage(img, 0, 0)
-};
+var context;
+var canvasWidth;
+var canvasHeight;
+var image_loaded;
+var img = new Image();
+var arr;
+var framesCount;
+let animationReq = null;
 
-// Set the number of frames we want to run.
-var frames = 160;
+function showEnteringPortalsLoader(){
+	showLoader("TemplateData/SPLASH_SCREEN2.png");
+}
 
-// Set and create our dot.
-var dot1 = { x: 0, y: 0, radius: 5, currentFrame: 0, incr: 0.1 };
-var dot2 = { x: 0, y: 0, radius: 5, currentFrame: 0, incr: 0.1 };
-var dot3 = { x: 0, y: 0, radius: 5, currentFrame: 0, incr: 0.1 };
-var arr = [dot1];
+function showLoader(imageSRC) {
+	var loader = document.getElementById('loader_canvas_div');
+	loader.style.display = "block";
+	
+	// Setup the canvas element.
+	var canvas = $("canvas.dots");
+	context = canvas[0].getContext("2d");
+	canvasWidth = canvas.width();
+	canvasHeight = canvas.height();
+	//canvas.attr({ height: canvasHeight, width: canvasWidth });
+	image_loaded = false;
+	img = new Image();
+	img.src = imageSRC;
+	img.onload = () => {
+	  image_loaded =true;
+	  context.drawImage(img, 0, 0)
+	};
 
-requestAnimationFrame(function() {
-        moveDot(740, 675)
-    })
+	// Set the number of frames we want to run.
+	framesCount = 160;
 
-var timeout = 150;
+	// Set and create our dot.
+	var dot1 = { x: 0, y: 0, radius: 5, currentFrame: 0, incr: 0.1 };
+	var dot2 = { x: 0, y: 0, radius: 5, currentFrame: 0, incr: 0.1 };
+	var dot3 = { x: 0, y: 0, radius: 5, currentFrame: 0, incr: 0.1 };
+	arr = [dot1];
 
-setTimeout(function () {
-	arr.push(dot2);
-}, timeout);
+	animationReq = requestAnimationFrame(function() {
+			moveDot(740, 675)
+		})
 
-setTimeout(function () {
-	arr.push(dot3);
-}, timeout * 2);
+	var timeout = 150;
+
+	setTimeout(function () {
+		arr.push(dot2);
+	}, timeout);
+
+	setTimeout(function () {
+		arr.push(dot3);
+	}, timeout * 2);
+}
+
+function closeLoader(){
+	var loader = document.getElementById('loader_canvas_div');
+	loader.style.display = "none";
+	if (animationReq != null){
+		//loader.remove();
+		cancelAnimationFrame(animationReq);
+		animationReq = null;
+		context = null;
+		image_loaded = false;
+		img = null;
+		arr = null;
+		framesCount = 0;
+	}
+}
 
 // This function moves the dot down and to the right in each frame.
 function moveDot(startX, startY) {
-
   // Clear the canvas so we can draw on it again.
   context.clearRect(0, 0, canvasWidth, canvasHeight);
 
@@ -51,7 +83,7 @@ function moveDot(startX, startY) {
 	
 	drawDot(startX, startY);
 
-requestAnimationFrame(function() {
+animationReq = requestAnimationFrame(function() {
         moveDot(startX, startY)
     })
 }
@@ -65,7 +97,7 @@ function calcDot(dot)
   // Move the current time forward by one frame.
   dot.currentFrame += 1;
   
-  if (dot.currentFrame == frames / 2)
+  if (dot.currentFrame == framesCount / 2)
   {
 	dot.incr *= -1;
 	dot.currentFrame = 0;
