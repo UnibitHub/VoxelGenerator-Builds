197cf5756436868cb3668bfa580e7345be21525a
v17alya <novosadvitalya@gmail.com>
2023-03-28 23:02:22 +0300

    Update scale_slider.js

--- betterspace-build/js/scale_slider.js
+++ betterspace-build/js/scale_slider.js
@@ -4,17 +4,23 @@ var loader_canvas = document.querySelector("#loader_canvas");
 var splash_screen = document.querySelector("#SPLASH_SCREEN");
 var defaultWidth = main_canvas.style.width;
 var defaultHeight = main_canvas.style.height;
+var defaultCanvasWidth = main_canvas.width;
+var defaultCanvasHeight = main_canvas.height;
 // var output = document.getElementById("demo");
 // output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-	main_canvas.style.width = (parseInt(defaultWidth) * (this.value) / 50) + "px";
-	main_canvas.style.height = (parseInt(defaultHeight) * (this.value) / 50) + "px";
+	var width = (parseInt(defaultWidth) * (this.value) / 50);
+	var height = (parseInt(defaultHeight) * (this.value) / 50);
+	var canvasWidth = (parseInt(defaultCanvasWidth) * (this.value) / 50);
+	var canvasHeight = (parseInt(defaultCanvasHeight) * (this.value) / 50);
+	main_canvas.style.width = width + "px";
+	main_canvas.style.height = height + "px";
+	main_canvas.width = canvasWidth;
+	main_canvas.height = canvasHeight;
 	loader_canvas.style.width = main_canvas.style.width;
 	loader_canvas.style.height = main_canvas.style.height;
 	splash_screen.style.width = main_canvas.style.width;
 	splash_screen.style.height = main_canvas.style.height;
-	console.log('w: ' + main_canvas.style.width);
-	console.log('h: ' + main_canvas.style.height);
 }
