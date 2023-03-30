8f3b2c6c6e3bb894c0027f7abff2ae028a33b0b4
v17alya <novosadvitalya@gmail.com>
2023-03-30 16:04:09 +0300

    *

--- new/betterspace-build/js/animated_loader.js
+++ new/betterspace-build/js/animated_loader.js
@@ -1,43 +1,94 @@
-// Setup the canvas element.
-var canvas = $("canvas.dots");
-var context = canvas[0].getContext("2d");
-var canvasWidth = canvas.width();
-var canvasHeight = canvas.height();
-canvas.attr({ height: canvasHeight, width: canvasWidth });
-var image_loaded = false;
-const img = new Image();
-img.src = "TemplateData/SPLASH_SCREEN.jpg";
-img.onload = () => {
-  image_loaded =true;
-  context.drawImage(img, 0, 0)
-};
+var context;
+var canvasWidth;
+var canvasHeight;
+var image_loaded;
+var img = new Image();
+var arr;
+var framesCount;
+let animationReq = null;
+var offsetX = 50;
+var dotColor = "#2800D7";
 
-// Set the number of frames we want to run.
-var frames = 160;
+function showEnteringPortalsLoader(){
+	showLoader("TemplateData/SPLASH_SCREEN_JPG.jpg");
+}
 
-// Set and create our dot.
-var dot1 = { x: 0, y: 0, radius: 5, currentFrame: 0, incr: 0.1 };
-var dot2 = { x: 0, y: 0, radius: 5, currentFrame: 0, incr: 0.1 };
-var dot3 = { x: 0, y: 0, radius: 5, currentFrame: 0, incr: 0.1 };
-var arr = [dot1];
+function showLoader(imageSRC) {
+	if (animationReq != null){
+		return;
+	}
+	
+	var loader = document.getElementById('loader_canvas_div');
+	loader.style.display = "block";
+	
+	// Setup the canvas element.
+	var canvas = $("canvas.dots");
+	context = canvas[0].getContext("2d");
+	canvasWidth = canvas.width();
+	canvasHeight = canvas.height();
+	//canvas.attr({ height: canvasHeight, width: canvasWidth });
+	image_loaded = false;
+	img = new Image();
+	img.src = imageSRC;
+	img.onload = () => {
+	  image_loaded =true;
+	  context.drawImage(img, 0, 0)
+	};
 
-requestAnimationFrame(function() {
-        moveDot(740, 675)
-    })
+	// Set the number of frames we want to run.
+	framesCount = 170;
+	
+	var incr = 0.5;
+	var dotRadius = 0;
 
-var timeout = 150;
+	// Set and create our dot.
+	var dot1 = { x: 0, y: 0, radius: dotRadius, currentFrame: 0, incr: incr, delayFrame: 0 };
+	var dot2 = { x: 0, y: 0, radius: dotRadius, currentFrame: 0, incr: incr, delayFrame: 0 };
+	var dot3 = { x: 0, y: 0, radius: dotRadius, currentFrame: 0, incr: incr, delayFrame: 0 };
+	var dot4 = { x: 0, y: 0, radius: dotRadius, currentFrame: 0, incr: incr, delayFrame: 0 };
+	var dot5 = { x: 0, y: 0, radius: dotRadius, currentFrame: 0, incr: incr, delayFrame: 0 };
+	arr = [dot1];
 
-setTimeout(function () {
-	arr.push(dot2);
-}, timeout);
+	animationReq = requestAnimationFrame(function() {
+			moveDot(535, 615)
+		})
 
-setTimeout(function () {
-	arr.push(dot3);
-}, timeout * 2);
+	var timeout = 180;
+
+	setTimeout(function () {
+		arr.push(dot2);
+	}, timeout);
+
+	setTimeout(function () {
+		arr.push(dot3);
+	}, timeout * 2);
+
+	setTimeout(function () {
+		arr.push(dot4);
+	}, timeout * 3);
+
+	setTimeout(function () {
+		arr.push(dot5);
+	}, timeout * 4);
+}
+
+function closeLoader(){
+	var loader = document.getElementById('loader_canvas_div');
+	loader.style.display = "none";
+	if (animationReq != null){
+		//loader.remove();
+		cancelAnimationFrame(animationReq);
+		animationReq = null;
+		context = null;
+		image_loaded = false;
+		img = null;
+		arr = null;
+		framesCount = 0;
+	}
+}
 
 // This function moves the dot down and to the right in each frame.
 function moveDot(startX, startY) {
-
   // Clear the canvas so we can draw on it again.
   context.clearRect(0, 0, canvasWidth, canvasHeight);
 
@@ -51,24 +102,43 @@ function moveDot(startX, startY) {
 	
 	drawDot(startX, startY);
 
-requestAnimationFrame(function() {
+animationReq = requestAnimationFrame(function() {
         moveDot(startX, startY)
     })
 }
 
 function calcDot(dot)
 {
-// Adjust the dot's x and y values down and to the right.
+  // Adjust the dot's x and y values down and to the right.
   dot.x += dot.incr;
   dot.y += dot.incr;
 
   // Move the current time forward by one frame.
   dot.currentFrame += 1;
   
-  if (dot.currentFrame == frames / 2)
+  
+  
+  if (dot.currentFrame == framesCount / 2)
   {
-	dot.incr *= -1;
-	dot.currentFrame = 0;
+	  if (dot.incr < 0)
+	  {
+		  dot.delayFrame += 1;
+		  dot.currentFrame -= 1;
+		  dot.x = 0;
+		  dot.y = 0;
+		  
+		  if (dot.delayFrame == 80)
+		  {
+			  dot.incr *= -1;
+			  dot.currentFrame = 0;
+			  dot.delayFrame = 0;
+		  }
+	  }
+	  else
+	  {
+		dot.incr *= -1;
+		dot.currentFrame = 0;
+	  }
   }
 }
 
@@ -76,8 +146,8 @@ function drawDot(startX, startY) {
   context.beginPath();
   for (var i = 0; i < arr.length; i++){
     var dot = arr[i];
-	context.rect(startX + (20 * i) - dot.x / 2, startY   - dot.x / 2, dot.radius + dot.x, dot.radius + dot.x, false);
+	context.rect(startX + (offsetX * i) - dot.x / 2, startY   - dot.x / 2, dot.radius + dot.x, dot.radius + dot.x, false);
   }
-  context.fillStyle = "#808080";
+  context.fillStyle = dotColor;
   context.fill();
 }
