88552be7f55bc8dfdebfc357395271133a3f9b9e
v17alya <novosadvitalya@gmail.com>
2023-04-08 15:16:27 +0300

    *

--- betterspace-build/js/discord.js
+++ betterspace-build/js/discord.js
@@ -5,7 +5,6 @@ function signWithDiscord(redirectUri, callback){
 	removeEventListener();
 	
 	const eventListener = event => {
-		console.log(event.data);
 		channel.removeEventListener('message', eventListener); 
 		callback(event.data);
     };
@@ -13,9 +12,7 @@ function signWithDiscord(redirectUri, callback){
 	this.eventListeners.push(eventListener); 
 	channel.addEventListener ('message', eventListener);
 
-	console.log("redirectUri: " + redirectUri);
 	var uri = "https://discord.com/api/oauth2/authorize?client_id=1092513317169414235&redirect_uri=" + redirectUri + "&response_type=code&scope=identify%20guilds%20email";
-	console.log(uri);
 	var disc_window = window.open(uri);
 }
 
@@ -23,20 +20,16 @@ function authWithDiscord(){
 	removeEventListener();
 	
 	const eventListener = event => {
-		console.log(event.data);
 		channel.removeEventListener('message', eventListener); 
     };
 	
 	this.eventListeners.push(eventListener); 
 	channel.addEventListener ('message', eventListener);
 	
-	console.log(window.location.port);
 	var port = window.location.port;
 	var port_string = port ? "%3A" + port : "";
 	var redirect_uri = "http%3A%2F%2F" + window.location.hostname + port_string  +"%2Fdiscord_wait.html";
 	var uri = "https://discord.com/api/oauth2/authorize?client_id=1092513317169414235&redirect_uri=" + redirect_uri + "&response_type=code&scope=identify%20guilds%20email";
-	console.log(redirect_uri);
-	console.log(uri);
 	var disc_window = window.open(uri);
 }
 
