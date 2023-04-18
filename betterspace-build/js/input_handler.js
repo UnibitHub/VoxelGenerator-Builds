025bf4c0f4efdb82fbe6285d8cdeb02b4bff12ee
v17alya <novosadvitalya@gmail.com>
2023-04-18 17:08:23 +0300

    Update input_handler.js

--- betterspace-build/js/input_handler.js
+++ betterspace-build/js/input_handler.js
@@ -1,8 +1,7 @@
 function subscribeOnKeyEvents(){
 	document.addEventListener('keyup', evt => {
-		console.log("keyup: " + evt.key);
 		if (evt.key === 'Escape') {
-			console.log("Escape: " + evt.repeat);
+			console.log("Escape1: " + evt.repeat);
 			if (!evt.repeat){
 				myGameInstance.SendMessage("JSInputHandler", "OnEscapeButtonPressed");
 				// Cancel the default action to avoid it being handled twice
@@ -10,4 +9,35 @@ function subscribeOnKeyEvents(){
 			}
 		}
 	},true);
+	document.addEventListener('keyup', evt => {
+		if (evt.key === 'Escape') {
+			console.log("Escape2: " + evt.repeat);
+			if (!evt.repeat){
+				myGameInstance.SendMessage("JSInputHandler", "OnEscapeButtonPressed");
+				// Cancel the default action to avoid it being handled twice
+				evt.preventDefault();
+			}
+		}
+	},false);
+	document.addEventListener('keyup', evt => {
+		if (evt.key === 'Escape') {
+			console.log("Escape3: " + evt.repeat);
+			evt.preventDefault();
+			if (!evt.repeat){
+				myGameInstance.SendMessage("JSInputHandler", "OnEscapeButtonPressed");
+				// Cancel the default action to avoid it being handled twice
+				
+			}
+		}
+	},false);
+	document.addEventListener('keyup', evt => {
+		if (evt.key === 'Escape') {
+			console.log("Escape4: " + evt.repeat);
+			if (!evt.repeat){
+				myGameInstance.SendMessage("JSInputHandler", "OnEscapeButtonPressed");
+				// Cancel the default action to avoid it being handled twice
+			}
+			evt.preventDefault();
+		}
+	},false);
 }
