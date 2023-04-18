0fd5b91286a15877ab3f7bb960207e6161da4a13
v17alya <novosadvitalya@gmail.com>
2023-04-18 23:29:44 +0300

    Update input_handler.js

--- betterspace-build/js/input_handler.js
+++ betterspace-build/js/input_handler.js
@@ -9,35 +9,12 @@ function subscribeOnKeyEvents(){
 			}
 		}
 	},true);
-	document.addEventListener('keyup', evt => {
-		if (evt.key === 'Escape') {
-			console.log("Escape2: " + evt.repeat);
-			if (!evt.repeat){
-				myGameInstance.SendMessage("JSInputHandler", "OnEscapeButtonPressed");
-				// Cancel the default action to avoid it being handled twice
-				evt.preventDefault();
-			}
-		}
-	},false);
-	document.addEventListener('keyup', evt => {
-		if (evt.key === 'Escape') {
-			console.log("Escape3: " + evt.repeat);
-			evt.preventDefault();
-			if (!evt.repeat){
-				myGameInstance.SendMessage("JSInputHandler", "OnEscapeButtonPressed");
-				// Cancel the default action to avoid it being handled twice
-				
-			}
-		}
-	},false);
-	document.addEventListener('keyup', evt => {
-		if (evt.key === 'Escape') {
-			console.log("Escape4: " + evt.repeat);
-			if (!evt.repeat){
-				myGameInstance.SendMessage("JSInputHandler", "OnEscapeButtonPressed");
-				// Cancel the default action to avoid it being handled twice
-			}
-			evt.preventDefault();
-		}
-	},false);
+	
+	var canvas = document.querySelector("#unity-canvas");
+	canvas.addEventListener("mousedown", onMouseDown, false);
+}
+ 
+function onMouseDown(event)
+{
+    window.focus();
 }
