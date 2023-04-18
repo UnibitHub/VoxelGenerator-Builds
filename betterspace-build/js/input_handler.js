dfd625952e42bb5c6b3f91df78085905bfa3cd35
v17alya <novosadvitalya@gmail.com>
2023-04-18 10:28:40 +0300

    Update input_handler.js

--- betterspace-build/js/input_handler.js
+++ betterspace-build/js/input_handler.js
@@ -2,11 +2,12 @@ function subscribeOnKeyEvents(){
 	document.addEventListener('keydown', evt => {
 		if (evt.key === 'Escape') {
 			console.log("Escape: " + evt.repeat);
-			myGameInstance.SendMessage("JSInputHandler", "OnEscapeButtonPressed");
 			
 			// Cancel the default action to avoid it being handled twice
 			evt.preventDefault();
+			
+			if (!evt.repeat)
+				myGameInstance.SendMessage("JSInputHandler", "OnEscapeButtonPressed");
 		}
-	});
-	//,true);
+	},true);
 }
