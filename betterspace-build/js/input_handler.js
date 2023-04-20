0847eac009ca138bf826a99c01597475ab7dcc16
v17alya <novosadvitalya@gmail.com>
2023-04-20 16:57:35 +0300

    *

--- betterspace-build/js/input_handler.js
+++ betterspace-build/js/input_handler.js
@@ -1,32 +1,7 @@
 function subscribeOnKeyEvents(){
-	document.addEventListener('keyup', evt => {
+	document.addEventListener('keydown', evt => {
 		if (evt.key === 'Escape') {
-			console.log("Escape1: " + evt.repeat);
-			if (!evt.repeat){
-				myGameInstance.SendMessage("JSInputHandler", "OnEscapeButtonPressed");
-				// Cancel the default action to avoid it being handled twice
-				evt.preventDefault();
-			}
+			myGameInstance.SendMessage("JSInputHandler", "OnEscapeButtonPressed");
 		}
-	},true);
-	
-	var canvas = document.querySelector("#unity-canvas");
-	canvas.addEventListener("mousedown", onMouseDown, false);
-	
-	document.onkeydown = myKeyDownHandler1;
-	canvas.onkeydown = myKeyDownHandler2;
+	});
 }
- 
-function onMouseDown(event)
-{
-	console.log('onMouseDown focus');
-    canvas.focus();
-}
-
-    function myKeyDownHandler1(){
-      console.log("A key down event took place within the document1!");
-    }
-	
-	function myKeyDownHandler2(){
-      console.log("A key down event took place within the document2!");
-    }
