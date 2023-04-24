ddc9f48ac23111b76dcde3057f8e8b8f349d91eb
v17alya <novosadvitalya@gmail.com>
2023-04-24 23:50:51 +0300

    Update amplitude.js

--- betterspace-build/js/amplitude.js
+++ betterspace-build/js/amplitude.js
@@ -1,66 +1,30 @@
 function sendEvent() {
-	var myBody = 
-	{
-	   "events":[
-		  {
-			  "user_properties":{
-				 "Server":"Prod",
-				 "Fullscreen":false,
-				 "ProcessorsCount":16,
-				 "VideoCard":"NVIDIA GeForce GTX 1650",
-				 "Min_RAM":16.0,
-				 "Browser":null,
-				 "Link_Open":"editor_MegaMod",
-				 "Room_ID":"SERVER-EDITOR_ROOM_0001",
-				 "Player_ID":"e6ba93c2-9534-4e6f-8d4f-2a864bef91c1",
-				 "Player_Name":"",
-				 "Skin_ID":null,
-				 "Login_Method":"No Authorized",
-				 "Email":"No Authorized"
-			  },
-			 "user_id":"675221f5d1a4121488d52ac6bbbaa532",
-			 "session_id":1653224978235020318,
-			 "insert_id":"638179658219672505_-417362788",
-			 "app_version":"1.1.35",
-			 "platform":"WindowsEditor",
-			 "event_type":"Hello_World",
-			 "time":1682364523002,
-			 // "event_properties":{
-				// "Ping":66,
-				// "Players_Count":1,
-				// "In_Room":true,
-				// "Timer":2.14748365E+09
-			 // }
-		  }
-	   ]
-	}
-	
 	// var myBody = 
 	// {
 	   // "events":[
 		  // {
-			  // // "user_properties":{
-				 // // "Server":"Prod",
-				 // // "Fullscreen":false,
-				 // // "ProcessorsCount":16,
-				 // // "VideoCard":"NVIDIA GeForce GTX 1650",
-				 // // "Min_RAM":16.0,
-				 // // "Browser":null,
-				 // // "Link_Open":"editor_MegaMod",
-				 // // "Room_ID":"SERVER-EDITOR_ROOM_0001",
-				 // // "Player_ID":"e6ba93c2-9534-4e6f-8d4f-2a864bef91c1",
-				 // // "Player_Name":"",
-				 // // "Skin_ID":null,
-				 // // "Login_Method":"No Authorized",
-				 // // "Email":"No Authorized"
-			  // // },
-			 // // "user_id":"675221f5d1a4121488d52ac6bbbaa532",
-			 // // "session_id":1653224978235020318,
-			 // // "insert_id":"638179608219672505_-417362788",
+			  // "user_properties":{
+				 // "Server":"Prod",
+				 // "Fullscreen":false,
+				 // "ProcessorsCount":16,
+				 // "VideoCard":"NVIDIA GeForce GTX 1650",
+				 // "Min_RAM":16.0,
+				 // "Browser":null,
+				 // "Link_Open":"editor_MegaMod",
+				 // "Room_ID":"SERVER-EDITOR_ROOM_0001",
+				 // "Player_ID":"e6ba93c2-9534-4e6f-8d4f-2a864bef91c1",
+				 // "Player_Name":"",
+				 // "Skin_ID":null,
+				 // "Login_Method":"No Authorized",
+				 // "Email":"No Authorized"
+			  // },
+			 // "user_id":"675221f5d1a4121488d52ac6bbbaa532",
+			 // "session_id":1653224978235020318,
+			 // "insert_id":"638179658219672505_-417362788",
 			 // "app_version":"1.1.35",
 			 // "platform":"WindowsEditor",
 			 // "event_type":"Hello_World",
-			 // // "time":1682364023002,
+			 // "time":1682364523002,
 			 // // "event_properties":{
 				// // "Ping":66,
 				// // "Players_Count":1,
@@ -70,6 +34,42 @@ function sendEvent() {
 		  // }
 	   // ]
 	// }
+	
+	var myBody = 
+	{
+	   "events":[
+		  {
+			  // "user_properties":{
+				 // "Server":"Prod",
+				 // "Fullscreen":false,
+				 // "ProcessorsCount":16,
+				 // "VideoCard":"NVIDIA GeForce GTX 1650",
+				 // "Min_RAM":16.0,
+				 // "Browser":null,
+				 // "Link_Open":"editor_MegaMod",
+				 // "Room_ID":"SERVER-EDITOR_ROOM_0001",
+				 // "Player_ID":"e6ba93c2-9534-4e6f-8d4f-2a864bef91c1",
+				 // "Player_Name":"",
+				 // "Skin_ID":null,
+				 // "Login_Method":"No Authorized",
+				 // "Email":"No Authorized"
+			  // },
+			 "user_id":"675221f5d1a4121488d52ac6bbbaa532",
+			 // "session_id":1653224978235020318,
+			 // "insert_id":"638179608219672505_-417362788",
+			 "app_version":"1.1.35",
+			 "platform":"WindowsEditor",
+			 "event_type":"Hello_World",
+			 // "time":1682364023002,
+			 // "event_properties":{
+				// "Ping":66,
+				// "Players_Count":1,
+				// "In_Room":true,
+				// "Timer":2.14748365E+09
+			 // }
+		  }
+	   ]
+	}
 	
 	fetch('https://dev-better-space-api.herokuapp.com/api/game/httpApi', {
     method: 'POST',
